a6b0873e6c5c79206478f9fabffa2417e82f3fd7
KenWheadon <ken@smartfirm.com>
2025-06-28 13:42:27 -0300

    Update styles

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ const CONFIG = {
     CHARACTER_PREFIX: "images/",
     CHARACTER_EXTENSION: ".png",
     LOCATION_PREFIX: "images/location-",
-    BACKGROUND: "images/location-ballroon.png",
+    BACKGROUND: "images/location-ballroom.png",
   },
 
   AUDIO: {
